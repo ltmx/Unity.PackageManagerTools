7ba62bac041bc96b856fb6bb7eefd8abd9d9bcc0
LTMX <47640688+LTMX@users.noreply.github.com>
2024-03-05 11:53:08 +0000

    Clamped max image size to max height in pixels (width is automatically set)

--- Editor/MarkdownRenderer/Editor/ImageExtension.cs
+++ Editor/MarkdownRenderer/Editor/ImageExtension.cs
@@ -9,7 +9,7 @@ namespace UIMarkdownRenderer
         {
             img.scaleMode = ScaleMode.ScaleToFit;
             img.sourceRect = new Rect(0, 0, img.image.width, img.image.height);
-            img.style.height = img.image.height * (img.resolvedStyle.width / img.image.width);
+            img.style.height = Mathf.Min(img.image.height * (img.resolvedStyle.width / img.image.width), img.image.height);
         }
     }
 }
